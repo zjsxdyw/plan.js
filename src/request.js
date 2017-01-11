4680fa9aeb3540f36d4f77a55c1bdb38eb3e6fc0
zjsxdyw <zjsxdyw@gmail.com>
2017-01-11 12:39:53 +0800

    Create request.js

---
+++ src/request.js
@@ -0,0 +1,40 @@
+function Request() {
+    var queue = [], state = 'idle', obj,
+        run = function () {
+            if(state === 'idle' && queue.length !== 0) {
+                obj = queue.shift();
+                state = 'request';
+                obj.workflow.validate(obj.message);
+            }
+        },
+        callback = function() {
+            if(state === 'request') return;
+            state = 'idle';
+            run();
+        };
+    return {
+        plan: function (message, workflow, front) {
+            if(front) {
+                queue.unshift({message: message, workflow: workflow});
+            } else {
+                queue.push({message: message, workflow: workflow});
+            }
+            run();
+        },
+        success: function(data) {
+            state = 'process';
+            obj.workflow.execute(obj.message, data);
+            callback();
+        },
+        error: function(data) {
+            state = 'process';
+            if(!obj.workflow.ignore) {
+                while(queue.length) {
+                    queue.shift();
+                }
+            }
+            obj.workflow.exception(obj.message, data);
+            callback();
+        }
+    }
+}
